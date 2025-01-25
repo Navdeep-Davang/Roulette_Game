9dd8f002afc66f3cb561fba4775eef0ce9cd1122
Navdeep Pandurang Davang <navdeepdavang@gmail.com>
2025-01-25 20:20:10 +0530

    Refactoring css of leaderboard

--- dir/components/roulette/LeaderBoard/index.tsx
+++ dir/components/roulette/LeaderBoard/index.tsx
@@ -4,7 +4,7 @@ const LeaderBoard = () => {
   return (
     <div className="leaderboard-container select-none p-6 justify-center items-center gap-8 flex flex-wrap">
       <div className="leaderboard-item w-full min-w-[200px] flex-col justify-center items-center">
-        <div className="leaderboard-win-2x w-full  p-2 justify-center items-center">
+        <div className="leaderboard-win-2x-red w-full  p-2 justify-center items-center">
           <div className="leaderboard-title text-center">Win 2X</div>
         </div>
         <div className="leaderboard-player-container w-full h-12  py-4 border-b flex-col justify-center items-center">
@@ -22,7 +22,7 @@ const LeaderBoard = () => {
       </div>
 
       <div className="leaderboard-item w-full min-w-[200px] flex-col justify-center items-center">
-        <div className="leaderboard-win-2x w-full p-2 justify-center items-center">
+        <div className="leaderboard-win-2x-black w-full p-2 justify-center items-center">
           <div className="leaderboard-title text-center">Win 2X</div>
         </div>
         <div className="leaderboard-player-container w-full h-12 py-4 border-b flex-col justify-center items-center">
